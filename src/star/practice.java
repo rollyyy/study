d33321b4aa0dba04ac92d36f58de00f521315bf3
김윤정 <kim785392@gmail.com>
2021-12-26 22:17:46 +0900

    firat

---
+++ src/star/practice.java
@@ -0,0 +1,25 @@
+package star;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		
+			for(int i = 1;i<=5;i++) {                               //외부 반복문 *****을 5번 반복                 
+				                                                                                                    
+				
+				
+				for(int z = 1;z<=5;z++) {                           //내부 반복문 ***** 진행
+					
+					System.out.print("*");
+					
+				}
+				
+				
+				System.out.println();                               //줄 바꿔줌
+				
+			}
+
+	}
+
+}
---
+++ src/star/practice2.java
@@ -0,0 +1,19 @@
+package star;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			
+			for(int z = 1; z<= 6 - i;z++ ) {
+				
+				
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}                                                            //역방향 별 찍기
